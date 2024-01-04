9989c13f1b6238d66cebb233fcf594666c1a85fc
Bogomila <bkatsarska@abv.bg>
2024-01-04 18:12:23 +0200

    updated section 16 + added section 17

---
+++ Section 17 - DLL - Doubly Linked List.py
@@ -0,0 +1,36 @@
+'''
+- Nodes have 2 references: to the prev and to the next one
+{
+    'value': 10,
+    'next': None,
+    'prev': None
+}
+'''
+
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+    def __str__(self):
+        return str(self.value)
+
+
+class DoublyLinedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+    def append(self, value):
+        new_node = Node(value)
+        if not self.head:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+        self.length += 1
