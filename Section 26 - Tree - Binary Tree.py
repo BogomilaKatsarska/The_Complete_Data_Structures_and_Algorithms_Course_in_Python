4d973324ddde20bf809b6bf7405bcbd1fc46e8fc
Bogomila <bkatsarska@abv.bg>
2024-01-18 15:49:41 +0200

    updated section 26

--- Section 26 - Tree - Binary Tree.py
+++ Section 26 - Tree - Binary Tree.py
@@ -23,13 +23,31 @@
         - binary tree is a family of data structure(BST, Heap tree, red black trees, syntax tree)
 
 4.Types of binary tree:
-    - Full BT: each node has either 2children or 0children
-    - Perfect BT: all non-leaf nodes have 2children and they are at the same depth
-    - Complete BT: all levels are perfect except the last one(not completely filled)
-    - Balanced BT: all leaf nodes are located in the same distance from the root node
+        - Full BT: each node has either 2children or 0children
+        - Perfect BT: all non-leaf nodes have 2children and they are at the same depth
+        - Complete BT: all levels are perfect except the last one(not completely filled)
+        - Balanced BT: all leaf nodes are located in the same distance from the root node
+
+5.Binary Tree Repr:
+        Root index: 1
+        Left child: cell[2x] => 2
+        Right child: cell[2x+1] => 3
+
+6.Traversal:
+        6.1.Depth First Search:
+            - preorder traversal
+                Root Node -> Left Subtree -> Right Subtree
+            - inorder traversal
+                Left Subtree -> Root Node -> Right Subtree
+            - post order traversal
+                Left Subtree -> Right Subtree -> Root Node
+        6.2.Breadth First Search:
+            - level order traversal
+                We visit level by level
 '''
+import queue
 
-
+'''
 class TreeNode:
     def __init__(self, data, children=[]):
         self.data = data
@@ -43,6 +61,7 @@ class TreeNode:
 
     def add_child(self, tree_node):
         self.children.append(tree_node)
+
 
 tree = TreeNode('Drinks', [])
 cold = TreeNode('Cold', [])
@@ -58,3 +77,43 @@ cold.add_child(fanta)
 hot.add_child(tea)
 hot.add_child(coffee)
 print(tree)
+'''
+
+class TreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left_child = None
+        self.right_child = None
+
+
+def preorder_traversal(root_node): #O(n)
+    if not root_node:
+        return
+    print(root_node.data)
+    preorder_traversal(root_node.left_child)
+    preorder_traversal(root_node.right_child)
+
+
+def inorder_traversal(root_node): #O(n)
+    if not root_node:
+        return
+    inorder_traversal(root_node.left_child)
+    print(root_node.data)
+    inorder_traversal(root_node.right_child)
+
+
+def postorder_traversal(root_node):
+    if not root_node:
+        return
+    postorder_traversal(root_node.left_child)
+    postorder_traversal(root_node.right_child)
+    print(root_node.data)
+
+def levelorder_traversal(root_node):
+    if not root_node:
+        return
+    else:
+        custom_queue = queue.Queue()
+        custom_queue.enqueue(root_node)
+        while not(custom_queue.is_empty()):
+            root = custom_queue.dequeue()
